3ed1946eabbfd2ebdd530c101c1a51bb4227964d
samirm00 <sammoham.be@gmail.com>
2021-01-15 13:50:35 +0100

    modified4-pooing2-.searchable.js

--- isolate/04-ooping-2/examples/3-searchable.js
+++ isolate/04-ooping-2/examples/3-searchable.js
@@ -28,33 +28,33 @@ const searchable = {
 };
 
 
-const allLength1 = searchable.all.length === 0;
-const matchingLength1 = searchable.matching.length === 0;
-console.assert(allLength1 && matchingLength1, 'Test 1');
+const allLength1 = searchable.state.all.length === 0;
+const matchingLength1 = searchable.state.matching.length === 0;
+console.assert(allLength1 && matchingLength1, "Test 1");
 
-searchable.addWord('hi!');
-searchable.addWord('hi');
-const allLength2 = searchable.all.length === 2;
-const matchingLength2 = searchable.matching.length === 2;
-console.assert(allLength2 && matchingLength2, 'Test 2');
+searchable.addWord("hi!");
+searchable.addWord("hi");
+const allLength2 = searchable.state.all.length === 2;
+const matchingLength2 = searchable.state.matching.length === 2;
+console.assert(allLength2 && matchingLength2, "Test 2");
 
-searchable.setQuery('!');
-const allLength3 = searchable.all.length === 2;
-const matchingLength3 = searchable.matching.length === 1;
-console.assert(allLength3 && matchingLength3, 'Test 3');
+searchable.setQuery("!");
+const allLength3 = searchable.state.all.length === 2;
+const matchingLength3 = searchable.state.matching.length === 1;
+console.assert(allLength3 && matchingLength3, "Test 3");
 
-searchable.addWord('hi?');
-const allLength4 = searchable.all.length === 3;
-const matchingLength4 = searchable.matching.length === 1;
-console.assert(allLength4 && matchingLength4, 'Test 4');
+searchable.addWord("hi?");
+const allLength4 = searchable.state.all.length === 3;
+const matchingLength4 = searchable.state.matching.length === 1;
+console.assert(allLength4 && matchingLength4, "Test 4");
 
-searchable.setQuery('hi');
-const allLength5 = searchable.all.length === 3;
-const matchingLength5 = searchable.matching.length === 3;
-console.assert(allLength5 && matchingLength5, 'Test 5');
+searchable.setQuery("hi");
+const allLength5 = searchable.state.all.length === 3;
+const matchingLength5 = searchable.state.matching.length === 3;
+console.assert(allLength5 && matchingLength5, "Test 5");
 
-searchable.setQuery('bye');
-const allLength6 = searchable.all.length === 3;
-const matchingLength6 = searchable.matching.length === 0;
-console.assert(allLength6 && matchingLength6, 'Test 6');
+searchable.setQuery("bye");
+const allLength6 = searchable.state.all.length === 3;
+const matchingLength6 = searchable.state.matching.length === 0;
+console.assert(allLength6 && matchingLength6, "Test 6");
 
